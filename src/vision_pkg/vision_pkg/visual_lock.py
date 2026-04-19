143db40352562ed53b70ceafd692912063062c56
vboxuser <vboxuser@Ubuntu1.myguest.virtualbox.org>
2026-04-19 18:28:05 +0530

    ALL 4 TASKS COMPLETED

--- src/vision_pkg/vision_pkg/visual_lock.py
+++ src/vision_pkg/vision_pkg/visual_lock.py
@@ -14,6 +14,7 @@ class VisualLock(Node):
         self.timer = self.create_timer(0.03, self.process_frame)
 
         self.state = "SEARCHING"
+        self.lost_frames = 0
 
     def process_frame(self):
 
@@ -25,26 +26,38 @@ class VisualLock(Node):
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        lower = np.array([20, 100, 100])
-        upper = np.array([35, 255, 255])
+        lower = np.array([40, 70, 70])
+        upper = np.array([85, 255, 255])
 
         mask = cv2.inRange(hsv, lower, upper)
+
+        mask = cv2.GaussianBlur(mask, (7, 7), 0)
+        mask = cv2.erode(mask, None, iterations=1)
+        mask = cv2.dilate(mask, None, iterations=2)
 
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         output = frame.copy()
         new_state = "SEARCHING"
 
-        if len(contours) == 0:
+        if len(contours) == 0 or max([cv2.contourArea(c) for c in contours], default=0) < 200:
 
-            new_state = "SEARCHING"
-            output = cv2.bitwise_not(frame)
+            self.lost_frames += 1
+
+            if self.lost_frames > 3:
+                new_state = "SEARCHING"
+                output = cv2.bitwise_not(frame)
+            else:
+                new_state = self.state
+                output = frame
 
         else:
 
+            self.lost_frames = 0
+
             largest = max(contours, key=cv2.contourArea)
 
-            if cv2.contourArea(largest) > 500:
+            if cv2.contourArea(largest) > 200:
 
                 x, y, w, h = cv2.boundingRect(largest)
 
@@ -56,20 +69,17 @@ class VisualLock(Node):
                 if cx < width / 3:
 
                     new_state = "ALIGNING LEFT"
-
                     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                     output = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
                 elif cx < 2 * width / 3:
 
                     new_state = "LOCKED"
-
                     output = frame
 
                 else:
 
                     new_state = "ALIGNING RIGHT"
-
                     edges = cv2.Canny(frame, 100, 200)
                     output = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 
@@ -77,7 +87,7 @@ class VisualLock(Node):
             self.get_logger().info(f"STATE: {new_state}")
             self.state = new_state
 
-        cv2.imshow("Visual Lock - Yellow Tracking", output)
+        cv2.imshow("Visual Lock - Green FSM", output)
         cv2.waitKey(1)
 
 
